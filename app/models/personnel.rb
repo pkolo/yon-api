140607f605c2e889db47abbf471c79c715bd9670
Patrick Kolodgy <pkolodgy@gmail.com>
2017-08-23 15:08:39 -0400

    Adds performer assoc to media

--- app/models/personnel.rb
+++ app/models/personnel.rb
@@ -1,3 +1,9 @@
 class Personnel < ActiveRecord::Base
   has_many :credits
+
+  has_many :songs, -> { distinct }, through: :credits, source: :creditable, source_type: 'Song'
+  has_many :albums, -> { distinct }, through: :credits, source: :creditable, source_type: 'Album'
+
+  has_many :songs_as_performer, ->(credit) { where 'credits.role = ?', "Artist" }, through: :credits, source: :creditable, source_type: 'Song'
+  has_many :albums_as_performer, ->(credit) { where 'credits.role = ?', "Artist" }, through: :credits, source: :creditable, source_type: 'Album'
 end
