340b0b31a1b9a44b4dd04de7cb604cd8709e7046
Patrick Kolodgy <pkolodgy@gmail.com>
2017-08-24 15:16:12 -0400

    Basic song serializer

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,7 @@
 class Song < ActiveRecord::Base
   belongs_to :album
   has_many :credits, as: :creditable, dependent: :destroy
+
+  has_many :artists, ->(credit) { where 'credits.role IN (?)', ["Artist"] }, through: :credits, source: :personnel
+  has_many :featured_artists, ->(credit) { where 'credits.role IN (?)', ["Featuring", "Duet"] }, through: :credits, source: :personnel
 end
--- app/serializers/song_serializer.rb
+++ app/serializers/song_serializer.rb
@@ -1,3 +1,19 @@
 class SongSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :url, :title, :year, :yachtski, :scores
+
+  has_many :artists
+  has_many :featured_artists
+
+  def url
+    "/songs/#{object.slug}"
+  end
+
+  def scores
+    {
+      jd: object.jd_score,
+      hunter: object.hunter_score,
+      steve: object.steve_score,
+      dave: object.dave_score
+    }
+  end
 end
