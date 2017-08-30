1be54cf7b74f5c048fe469369af1203fdf3b91b2
Patrick Kolodgy <pkolodgy@gmail.com>
2017-08-30 07:49:36 -0400

    Adds credit assoc to album

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,4 +1,17 @@
 class Album < ActiveRecord::Base
   has_many :songs
   has_many :credits, as: :creditable, dependent: :destroy
+  has_many :artists, ->(credit) { where 'credits.role IN (?)', ["Artist"] }, through: :credits, source: :personnel
+  has_many :featured_artists, ->(credit) { where 'credits.role IN (?)', ["Featuring", "Duet"] }, through: :credits, source: :personnel
+
+  def players
+    query = <<-SQL
+      SELECT p.id, p.name, p.yachtski, concat('/personnel/', p.slug) AS url, string_agg(c.role, ', ') AS roles
+      FROM personnels p JOIN credits c ON p.id=c.personnel_id
+      WHERE c.creditable_id=#{self.id} AND c.creditable_type='#{self.class}' AND c.role NOT IN ('Artist', 'Duet', 'Featuring')
+      GROUP BY p.id
+      ORDER BY p.yachtski DESC
+      SQL
+    ActiveRecord::Base.connection.execute(query)
+  end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,6 @@
 class Song < ActiveRecord::Base
   belongs_to :album
   has_many :credits, as: :creditable, dependent: :destroy
-
   has_many :artists, ->(credit) { where 'credits.role IN (?)', ["Artist"] }, through: :credits, source: :personnel
   has_many :featured_artists, ->(credit) { where 'credits.role IN (?)', ["Featuring", "Duet"] }, through: :credits, source: :personnel
 
@@ -9,7 +8,7 @@ class Song < ActiveRecord::Base
     query = <<-SQL
       SELECT p.id, p.name, p.yachtski, concat('/personnel/', p.slug) AS url, string_agg(c.role, ', ') AS roles
       FROM personnels p JOIN credits c ON p.id=c.personnel_id
-      WHERE c.creditable_id=#{self.id} AND c.creditable_type='Song' AND c.role NOT IN ('Artist', 'Duet', 'Featuring')
+      WHERE c.creditable_id=#{self.id} AND c.creditable_type='#{self.class}' AND c.role NOT IN ('Artist', 'Duet', 'Featuring')
       GROUP BY p.id
       ORDER BY p.yachtski DESC
       SQL
