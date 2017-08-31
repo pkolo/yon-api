41dace490dc5220c531a7200eceff894698a1944
Patrick Kolodgy <pkolodgy@gmail.com>
2017-08-31 17:00:38 -0400

    Creates song create action and route

--- app/controllers/api/v1/songs_controller.rb
+++ app/controllers/api/v1/songs_controller.rb
@@ -8,4 +8,20 @@ class Api::V1::SongsController < Api::V1::ApiController
     @song = Song.find(params[:id])
     render json: @song, serializer: ExtendedSongSerializer
   end
+
+  def create
+    @episode = Episode.find(params[:episode_id])
+    @song = Song.new(song_params)
+    if @song.save
+      @episode.songs << @song
+      render json: @song, status: :created
+    else
+      render json: {errors: @song.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def song_params
+      params.require(:song).permit(:title, :year, :jd_score, :dave_score, :hunter_score, :steve_score, personnel_attributes: => [:name])
+    end
 end
