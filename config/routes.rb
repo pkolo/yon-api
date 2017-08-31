434fb746738938a2c1aa2d96f1381822fa0a0168
Patrick Kolodgy <pkolodgy@gmail.com>
2017-08-31 14:55:32 -0400

    Creates session routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
       resources :albums, only: [:show]
       resources :personnel, only: [:show]
       resources :episodes, only: [:show]
+
+      scope :format => true, :constraints => { :format => 'json' } do
+        post   "/login"       => "sessions#create"
+        delete "/logout"      => "sessions#destroy"
+      end
     end
   end
 
