26b065aaf7058da7c29e5e29e77c7b5d688d1cfa
Patrick Kolodgy <pkolodgy@gmail.com>
2017-09-02 12:51:45 -0400

    Finds or creates assoc personnel

--- app/models/credit.rb
+++ app/models/credit.rb
@@ -1,4 +1,17 @@
 class Credit < ActiveRecord::Base
+  before_validation :find_or_initialize_personnel
+
   belongs_to :personnel
-  belongs_to :creditable, polymorphic: true
+  belongs_to :creditable, polymorphic: true, optional: true
+
+  accepts_nested_attributes_for :personnel
+
+  validates :role, presence: true
+  validates :personnel, presence: true
+
+  def find_or_initialize_personnel
+    # Find or initialize the personnel by name
+    self.personnel = Personnel.find_or_initialize_by(name: self.personnel.name)
+  end
+
 end
