9b11c166f545b86f9e7c5143281106beba0a38f8
Patrick Kolodgy <pkolodgy@gmail.com>
2017-09-02 15:34:17 -0400

    Updates personnel yachtski on credit create or destroy

--- app/models/credit.rb
+++ app/models/credit.rb
@@ -1,7 +1,7 @@
 class Credit < ActiveRecord::Base
   before_validation :find_or_initialize_personnel
 
-  belongs_to :personnel
+  belongs_to :personnel, touch: true
   belongs_to :creditable, polymorphic: true, optional: true
 
   accepts_nested_attributes_for :personnel
@@ -9,9 +9,11 @@ class Credit < ActiveRecord::Base
   validates :role, presence: true
   validates :personnel, presence: true
 
-  def find_or_initialize_personnel
-    # Find or initialize the personnel by name
-    self.personnel = Personnel.find_or_initialize_by(name: self.personnel.name)
-  end
+  private
+
+    def find_or_initialize_personnel
+      # Find or initialize the personnel by name
+      self.personnel = Personnel.find_or_initialize_by(name: self.personnel.name)
+    end
 
 end
