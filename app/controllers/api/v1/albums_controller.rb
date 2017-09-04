052b638fadc8845dd150078ef1deb263df9a9237
Patrick Kolodgy <pkolodgy@gmail.com>
2017-09-04 10:19:47 -0400

    Creates album through song

--- app/controllers/api/v1/albums_controller.rb
+++ app/controllers/api/v1/albums_controller.rb
@@ -1,8 +1,28 @@
 class Api::V1::AlbumsController < ApplicationController
+  before_action :require_login, only: [:create]
+  before_action :find_song, only: [:create]
 
   def show
     @album = Album.find(params[:id])
     render json: @album, serializer: ExtendedAlbumSerializer
   end
+
+  # Nested through song
+  def create
+    
+  end
+
+  private
+    def album_params
+      params.require(:album).permit(:discog_id)
+    end
+
+    def song_id_params
+      params.require(:song_id)
+    end
+
+    def find_song
+      @song = Song.find(song_id_params)
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,17 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :songs, only: [:index, :show]
       resources :albums, only: [:show]
       resources :personnel, only: [:show]
+      
+      resources :songs, only: [:index, :show] do
+        resources :albums, only: [:create]
+      end
+
       resources :episodes, only: [:show, :create] do
         resources :songs, only: [:create]
       end
+
       post   "/login"       => "sessions#create"
       delete "/logout"      => "sessions#destroy"
     end
