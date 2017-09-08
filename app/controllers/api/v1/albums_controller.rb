a1da700c302469467bb5b99936f493caf7db4e68
Patrick Kolodgy <pkolodgy@gmail.com>
2017-09-08 16:51:58 -0400

    Creates album and credits from response

--- app/controllers/api/v1/albums_controller.rb
+++ app/controllers/api/v1/albums_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::AlbumsController < Api::V1::ApiController
+
   before_action :require_login, only: [:create]
   before_action :find_song, only: [:create]
 
@@ -9,12 +10,21 @@ class Api::V1::AlbumsController < Api::V1::ApiController
 
   # Nested through song
   def create
-    @album = Album.find_or_initialize_by(album_params)
-    @song.album = @album
-    if @album.save
-      binding.pry
+    @album = Album.find_by(album_params)
+    discogs = DiscogsApi.new(type: "release", id: album_params["discog_id"])
+    album_data = discogs.get_data
+    adapter = DiscogsAdapter.new
+    params = adapter.generate_album_params_from(album_data, @song)
+    binding.pry
+    if @album
+      render json: @album, status: :created
     else
-      render json: {errors: @album.errors.full_messages}, status: :unprocessable_entity
+      @album = Album.new(params)
+      if @album.save
+        render json: @album, status: :created
+      else
+        render json: {errors: @album.errors.full_messages}, status: :unprocessable_entity
+      end
     end
   end
 
