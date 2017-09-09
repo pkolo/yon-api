4f31a1415f66869ec4ebebe1be3ce7a59f03de80
Patrick Kolodgy <pkolodgy@gmail.com>
2017-09-09 12:37:22 -0400

    Adapts basic album data to params from API payload

--- app/adapters/discogs_adapter.rb
+++ app/adapters/discogs_adapter.rb
@@ -1,25 +1,51 @@
 class DiscogsAdapter
+  include StringUtilities
+  include TrackMatchUtilities
 
-    def generate_album_params_from(payload, song)
-      {
-        "discog_id": payload["id"],
-        "title": payload["title"],
-        "year": payload["year"],
-        "credits_attributes": generate_credits_from(payload)
+  def generate_album_params_from(payload, song)
+    song_data = get_song_data_from(payload["tracklist"], song)
+    binding.pry
+    {
+      "discog_id": payload["id"],
+      # "title": payload["title"],
+      "year": payload["year"],
+      "artist_credits_attributes": generate_artist_credits_from(payload["artists"]),
+      "credits_attributes": generate_credits_from(payload["extraartists"])
+    }
+  end
+
+  def generate_artist_credits_from(credit_list)
+    credit_list.each_with_object([]) do |credit, memo|
+      memo << {
+        "role": "Artist",
+        "personnel_attributes": {
+          "name": remove_parens(credit["name"]),
+          "discog_id": credit["id"]
+        }
       }
     end
+  end
 
-    def generate_credits_from(payload)
-      payload["extraartists"].each_with_object([]) do |credit, memo|
+  def generate_credits_from(credit_list, type="release")
+    credit_list.each_with_object([]) do |credit, memo|
+      if credit["tracks"].empty? && type == "release"
         credit["role"].split(', ').each do |role|
           memo << {
             "role": role,
             "personnel_attributes": {
-              "name": credit["name"]
+              "name": remove_parens(credit["name"]),
+              "discog_id": credit["id"]
             }
           }
         end
       end
     end
+  end
+
+  def get_song_data_from(tracklist, song)
+    tracklist.find do |track|
+      track if is_match?(track["title"], song.title)
+    end
+  end
 
 end
