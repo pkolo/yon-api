0e6ae20b22c42f51ed4d187ed2d6e1e57f17b3d9
Patrick Kolodgy <pkolodgy@gmail.com>
2017-11-14 08:17:26 -0500

    Add id to serializer output

--- app/serializers/mini_song_serializer.rb
+++ app/serializers/mini_song_serializer.rb
@@ -1,5 +1,5 @@
 class MiniSongSerializer < ActiveModel::Serializer
-  attributes :url, :title, :yachtski
+  attributes :id, :url, :title, :yachtski
 
   has_many :artists
   has_many :featured_artists
