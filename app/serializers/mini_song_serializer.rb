7c8cddae8fa3fb96fbfd6cb95aefbd80e2f68e4f
Patrick Kolodgy <pkolodgy@gmail.com>
2017-11-20 12:46:52 -0500

    Restore mini serializers

---
+++ app/serializers/mini_song_serializer.rb
@@ -0,0 +1,10 @@
+class MiniSongSerializer < ActiveModel::Serializer
+  attributes :id, :resource_url, :title, :yachtski
+
+  has_many :artists
+  has_many :featured_artists
+
+  def resource_url
+    "/albums/#{object.id}"
+  end
+end
