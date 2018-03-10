81b2d3e22857ee4c2e75f408bb71bde31995af2b
Patrick Kolodgy <pkolodgy@gmail.com>
2018-03-10 16:42:39 -0500

    Add date and title to episodes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180310210535) do
+ActiveRecord::Schema.define(version: 20180310214144) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,8 @@ ActiveRecord::Schema.define(version: 20180310210535) do
     t.string "data_id"
     t.bigint "show_id"
     t.integer "episode_no"
+    t.date "air_date"
+    t.string "title"
     t.index ["show_id"], name: "index_episodes_on_show_id"
   end
 
