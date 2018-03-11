8dce5f523570d6d705b504e29dfff1ae21469cd8
Patrick Kolodgy <pkolodgy@gmail.com>
2018-03-11 10:07:42 -0400

    Add new episode data to serializers

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,5 +1,6 @@
 class Episode < ActiveRecord::Base
   has_and_belongs_to_many :songs
+  belongs_to :show
 
   validates :number, presence: true, uniqueness: true
 end
--- app/serializers/song_serializer.rb
+++ app/serializers/song_serializer.rb
@@ -1,8 +1,9 @@
 class SongSerializer < ActiveModel::Serializer
-  attributes :id, :resource_url, :title, :track_no, :yt_id, :year, :yachtski, :scores, :episode
+  attributes :id, :resource_url, :title, :track_no, :yt_id, :year, :yachtski, :scores
 
   has_many :artists
   has_many :featured_artists
+  has_many :episodes
 
   def resource_url
     "/songs/#{object.id}"
@@ -15,9 +16,5 @@ class SongSerializer < ActiveModel::Serializer
       steve: object.steve_score,
       dave: object.dave_score
     }
-  end
-
-  def episode
-    object.episodes.first
   end
 end
