48cb558db5ce8e66f85288e7750551b155878ff8
pkolo <pkolodgy@gmail.com>
2018-07-29 09:54:39 -0400

    Create episodes index action

--- app/controllers/api/v1/episodes_controller.rb
+++ app/controllers/api/v1/episodes_controller.rb
@@ -1,6 +1,11 @@
 class Api::V1::EpisodesController < Api::V1::ApiController
   before_action :require_login, only: [:create, :update]
 
+  def index
+    @episodes = Episode.all.order('id DESC')
+    render json: @episodes
+  end
+
   def show
     @episode = Episode.find(params[:id])
     render json: @episode, serializer: ExtendedEpisodeSerializer
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
         post '/albums/search' => 'albums#search'
       end
 
-      resources :episodes, only: [:show, :create, :update] do
+      resources :episodes, only: [:index, :show, :create, :update] do
         resources :songs, only: [:create]
       end
 
