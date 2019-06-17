69cb07c50554a00f2468fb676c1ba0cbd24d5206
pkolo <pkolodgy@gmail.com>
2019-06-17 06:46:08 -0400

    Fix request controller

--- app/controllers/api/v1/song_requests_controller.rb
+++ app/controllers/api/v1/song_requests_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::SongRequestsController < Api::V1::ApiController
   def create
     song_request = SongRequest.new(song_request_params)
-    song_request.source = "form"
 
     if song_request.save
       render json: song_request
@@ -13,6 +12,6 @@ class Api::V1::SongRequestsController < Api::V1::ApiController
   private
 
   def song_request_params
-    params.require(:song_request).permit(:name, :title, :artist, :link, :message)
+    params.require(:song_request).permit(:name, :title, :artist, :link, :message, :source)
   end
 end
