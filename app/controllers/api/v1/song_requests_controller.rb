2c78d089510d1381df198a90d4548abc76583368
pkolo <pkolodgy@gmail.com>
2019-06-16 22:12:14 -0400

    Fix song request controller

--- app/controllers/api/v1/song_requests_controller.rb
+++ app/controllers/api/v1/song_requests_controller.rb
@@ -1,8 +1,9 @@
 class Api::V1::SongRequestsController < Api::V1::ApiController
   def create
-    song_request = SongRequest.create(song_request_params)
+    song_request = SongRequest.new(song_request_params)
+    song_request.source = form
 
-    if song_request
+    if song_request.save
       render json: song_request
     else
       render json: { errors: song_request.errors.full_messages }, status: :unprocessable_entity
