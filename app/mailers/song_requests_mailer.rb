1ba0f95808c24d0696516cee277a50f44b51560a
pkolo <pkolodgy@gmail.com>
2019-06-18 07:39:13 -0400

    Send mailer immediately

--- app/mailers/song_requests_mailer.rb
+++ app/mailers/song_requests_mailer.rb
@@ -7,4 +7,13 @@ class SongRequestsMailer < ApplicationMailer
       to: ENV["MAILER_TO"],
       subject: "Yacht Or Nyacht Request Digest #{Time.current.strftime('%m/%d/%Y')}" )
   end
+
+  def send_request(request)
+    @request = request
+
+    mail(
+      from: ENV["MAILER_FROM"],
+      to: ENV["MAILER_TO"],
+      subject: "Yacht Or Nyacht Request /  '#{@request.title}' by #{@request.artist}" )
+  end
 end
--- app/models/song_request.rb
+++ app/models/song_request.rb
@@ -1,3 +1,11 @@
 class SongRequest < ActiveRecord::Base
   scope :undigested, -> { where(digested: false) }
+
+  after_create :send_mailer
+
+  private
+
+  def send_mailer
+    SongRequestsMailer.send_request(self).deliver_later
+  end
 end
